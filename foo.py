092e819f8c9dd0e9b01f46016764726a0a9efb59
Kaitlyn Landgraf <kaitlynl@stanford.edu>
2016-05-18 19:30:13 -0700

    first

--- foo.py
+++ foo.py
@@ -1,6 +1,27 @@
-from operator import itemgetter
-import csv
-SCORESHEET = '.static/data/SCORESHEET.CSV'
+from helpers import get_legislators, get_ziplookups
+from helpers import filter_by_state, filter_by_zipcode, sort_by_criteria
+
+
+# store the datarows in memory
+legislators = get_legislators()
+ziplookups = get_ziplookups()
+
+
+# this function is the only one that app.py needs to know about
+def just_do_it(state="", zipcode="", sortby="alpha"):
+    matched_rows = []
+    datarows = legislators
+    # first, filter
+    if zipcode:
+        filteredrows = filter_by_zipcode(zipcode, legislators, ziplookups)
+    else:
+        # by default, search by state abbreviation
+        filteredrows = filter_by_state(state, legislators)
+    # then, sort and return the result
+    # remember to pass in filteredrows, not legislators
+    return sort_by_criteria(sortby, filteredrows)
+
+
 
 def print_record_count():
-    print("there are", len(SCORESHEET), "rows.")
+    print("There are", len(legislators), 'rows.')
